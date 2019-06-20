9b2e1bb124abf85a276ccfcfe88c2aa6aab9e40c
dao huy duc <ducanime1@gmail.com>
2019-06-20 22:46:52 +0700

    update BTVN

--- BTVN_02/src/com/topica/btvn02/WordProcessor.java
+++ BTVN_02/src/com/topica/btvn02/WordProcessor.java
@@ -4,5 +4,6 @@ import java.util.Map;
 
 public interface WordProcessor {
     Map<String, Integer> filterWords(String content);
-    Map<String, Integer>  sortByFrequencyAsc(Map<String, Integer> map);
+
+    Map<String, Integer> sortByFrequencyAsc(Map<String, Integer> map);
 }
--- BTVN_02/src/com/topica/btvn02/WordReader.java
+++ BTVN_02/src/com/topica/btvn02/WordReader.java
@@ -1,6 +1,5 @@
 package com.topica.btvn02;
 
-import java.io.IOException;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
