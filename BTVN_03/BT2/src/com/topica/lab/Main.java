0fd7a1c638eee76a175d44c0712e2b7c0f550255
dao huy duc <huyducactvn@gmail.com>
2019-06-26 09:52:13 +0700

    update btvn

--- BTVN_03/BT2/src/com/topica/lab/Main.java
+++ BTVN_03/BT2/src/com/topica/lab/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         Class<?> studentClass = Student.class;
         try {
-            changeAttribute(studentClass, ATTRIBUTE_NAME);
+            changeAttributeValue(studentClass, ATTRIBUTE_NAME);
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
@@ -20,17 +20,20 @@ public class Main {
         }
     }
 
-    private static void changeAttribute(@NotNull Class<?> clazz, String attributeName) throws NoSuchFieldException, IllegalAccessException {
+    private static void changeAttributeValue(@NotNull Class<?> clazz, String attributeName) throws NoSuchFieldException, IllegalAccessException {
         Field field = clazz.getDeclaredField(attributeName);
         field.setAccessible(true);
 
-        modifiersField(field);
+        // modify field
+        modifyField(field);
         System.out.println("Before change: " + attributeName + " = " + field.get(clazz));
+
+        // Change value
         field.set(null, "172009990");
         System.out.println("After change: " + attributeName + " = " + field.get(clazz));
     }
 
-    private static void modifiersField(Field field) throws NoSuchFieldException, IllegalAccessException {
+    private static void modifyField(Field field) throws NoSuchFieldException, IllegalAccessException {
         Field modifiersField = Field.class.getDeclaredField(MODIFIERS);
         modifiersField.setAccessible(true);
         modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
