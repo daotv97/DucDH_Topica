f337d5f16cbf99d91e2ea2675efb36dab70a5618
dao huy duc <huyducactvn@gmail.com>
2019-06-26 10:21:39 +0700

    update btvn

--- BTVN_03/BT1/src/com/topica/lab/Main.java
+++ BTVN_03/BT1/src/com/topica/lab/Main.java
@@ -1,5 +1,7 @@
 package com.topica.lab;
 
+import com.sun.istack.internal.NotNull;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
@@ -8,30 +10,44 @@ public class Main {
 
     public static void main(String[] args) {
         Class<?> studentClass = Student.class;
+
         // The attribute have annotations
         System.out.println("- Attributes: ");
-        Arrays.stream(studentClass.getDeclaredFields())
+        getAttributeName(studentClass);
+
+        // The method have annotations
+        System.out.printf("\n- Methods: \n");
+        getMethodName(studentClass);
+
+        // The attribute field is "final"
+        System.out.printf("\n- Static final: \n");
+        getAttributeNameIsFinal(studentClass);
+
+    }
+
+    private static void getAttributeName(@NotNull Class<?> clazz) {
+        Arrays.stream(clazz.getDeclaredFields())
                 .forEach(field -> {
                     if (field.getAnnotations().length > 0) {
                         System.out.println("+ attribute: " + field.getName());
                     }
                 });
+    }
 
-        // The method have annotations
-        System.out.printf("\n- Methods: \n");
-        Arrays.stream(studentClass.getMethods())
+    private static void getMethodName(@NotNull Class<?> clazz) {
+        Arrays.stream(clazz.getMethods())
                 .forEach(method -> {
                     if (method.getAnnotations().length > 0) {
                         System.out.println("+ method: " + method.getName());
                     }
                 });
+    }
 
-        // Get attribute is "static final"
-        System.out.printf("\n- Static final: \n");
-        Arrays.stream(studentClass.getDeclaredFields()).forEach(field -> {
+    private static void getAttributeNameIsFinal(@NotNull Class<?> clazz) {
+        Arrays.stream(clazz.getDeclaredFields()).forEach(field -> {
             if (Modifier.isFinal(field.getModifiers())) {
                 try {
-                    Field f = studentClass.getDeclaredField(field.getName());
+                    Field f = clazz.getDeclaredField(field.getName());
                     f.setAccessible(true);
                     System.out.println("+ " + field.getName() + " = " + f.get(new Student()));
                 } catch (NoSuchFieldException e) {
@@ -41,7 +57,5 @@ public class Main {
                 }
             }
         });
-
     }
-
 }
