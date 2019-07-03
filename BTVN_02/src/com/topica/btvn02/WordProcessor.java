8ad5aa0ee61b6c8b2a4763bd9a52e26d94b1e87b
huyductopica <52006940+huyductopica@users.noreply.github.com>
2019-07-04 00:40:30 +0700

    update

--- BTVN_02/src/com/topica/btvn02/WordProcessor.java
+++ BTVN_02/src/com/topica/btvn02/WordProcessor.java
@@ -1,8 +1,9 @@
 package com.topica.btvn02;
+
 import java.util.Map;
 
 public interface WordProcessor {
     Map<String, Integer> filterWords(String content);
 
-    Map<String, Integer> sortByFrequencyAsc(Map<String, Integer> map);
+    Map<String, Integer> sortByFrequencyDesc(Map<String, Integer> map);
 }
