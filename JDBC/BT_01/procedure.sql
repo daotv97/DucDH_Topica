d0a9bfeb199cd7ec5508daa14610d99af32b6d79
Dao Huy Duc <huyducactvn@gmail.com>
2019-07-17 02:33:22 +0700

    update btvn

--- JDBC/BT_01/procedure.sql
+++ JDBC/BT_01/procedure.sql
@@ -1,8 +1,16 @@
 # Find all user account.
 
 DELIMITER $$
-CREATE PROCEDURE findAll()
+CREATE PROCEDURE findById()
 BEGIN
-    SELECT `us`.id, `us`.username, `us`.password, `us`.age, `us`.email, `us`.status FROM user as us;
+    SELECT `us`.id, `us`.username, `us`.password, `us`.age, `us`.email, `us`.status
+    FROM user as `us`;
 END $$
 DELIMITER ;
+
+CREATE PROCEDURE findById(id LONG)
+BEGIN
+    SELECT `us`.id, `us`.username, `us`.password, `us`.age, `us`.email, `us`.status
+    FROM user as `us`
+    WHERE `us`.id = id;
+END
