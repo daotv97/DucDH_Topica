19825edc2addde4ebe2ce4374532de78c5a65730
dao huy duc <huyducactvn@gmail.com>
2019-07-17 11:47:57 +0700

    update

--- JDBC/BT_01/src/main/java/com/topica/control/Main.java
+++ JDBC/BT_01/src/main/java/com/topica/control/Main.java
@@ -15,7 +15,23 @@ public class Main {
         if (connection != null) {
             UserRepository userRepository = new UserRepository(connection);
             Set<User> users = userRepository.findAll();
-            users.stream().forEach(System.out::println);
+            formatTableOutput(users);
         }
+    }
+
+    private static void formatTableOutput(Set<User> users) {
+        String leftAlignFormat = "| %-4d | %-15s | %-15s | %-4d | %-32s | %-10s |%n";
+
+        System.out.format("+------+-----------------+-----------------+------+----------------------------------+------------+%n");
+        System.out.format("| ID   | USERNAME        | PASSWORD        | AGE  | EMAIL                            | STATUS     |%n");
+        System.out.format("+------+-----------------+-----------------+------+----------------------------------+------------+%n");
+        users.stream().forEach(user -> System.out.format(leftAlignFormat
+                , user.getId()
+                , user.getUsername()
+                , user.getPassword()
+                , user.getAge()
+                , user.getEmail()
+                , user.isStatus()));
+        System.out.format("+------+-----------------+-----------------+------+----------------------------------+------------+%n");
     }
 }
