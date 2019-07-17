05b85db2118166d713a1481ab84ad1f3b20e2319
dao huy duc <huyducactvn@gmail.com>
2019-07-17 11:57:14 +0700

    update

--- JDBC/BT_01/procedure.sql
+++ JDBC/BT_01/procedure.sql
@@ -8,9 +8,11 @@ BEGIN
 END $$
 DELIMITER ;
 
+DELIMITER $$
 CREATE PROCEDURE findById(id LONG)
 BEGIN
     SELECT `us`.id, `us`.username, `us`.password, `us`.age, `us`.email, `us`.status
     FROM user as `us`
     WHERE `us`.id = id;
-END
+END $$
+DELIMITER ;
